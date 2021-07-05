bc59433f87dc5f0e92471740d58946474043bc1d
Jared Matta <cherokeedreemer@gmail.com>
2021-07-04 21:18:30 -0600

    keep going

---
+++ aa-store/src/page/shop/shoppage.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import Shop_Data from './shop.data';
+
+class ShopPage extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            collections: Shop_Data
+        }
+    }
+
+    render() {
+        return (
+            <div
+        )
+    }
+};
+
+export default ShopPage
