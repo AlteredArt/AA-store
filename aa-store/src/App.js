ed2724ed492d7da8501f737181319591301110c4
Jared Matta <cherokeedreemer@gmail.com>
2021-07-05 18:57:25 -0600

    adding shop page and item preview

--- aa-store/src/App.js
+++ aa-store/src/App.js
@@ -1,15 +1,10 @@
 import React from 'react';
-import HomePage from './page/homepage/homepage.component';
-import './App.css';
 import { Switch, Route } from 'react-router-dom';
+import './App.css';
 
-const HatsPage = () => (
-  <div>
-    <h1>
-      HATS PAGE
-    </h1>
-  </div>
-)
+import HomePage from './page/homepage/homepage.component';
+import ShopPage from './page/shop/shop';
+
 
 function App() {
   return (
@@ -17,7 +12,7 @@ function App() {
       <h1 className="title">Altered Art</h1>
       <Switch>
         <Route exact path="/" component={HomePage}/>
-        <Route path="/hats" component={HatsPage}/>
+        <Route path="/shop" component={ShopPage}/>
       </Switch>
     </div>
   );
