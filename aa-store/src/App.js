1ea589a5a4244098bd3c6d60f68b89d820c82b5f
Jared Matta <cherokeedreemer@gmail.com>
2021-07-08 09:21:14 -0600

    added dropdown

--- aa-store/src/App.js
+++ aa-store/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route, Redirect } from 'react-router-dom';
 import './App.css';
 import { connect } from 'react-redux';
 import HomePage from './page/homepage/homepage.component';
@@ -8,6 +8,7 @@ import Header from './component/header/header';
 import EntryPage from './page/entry/entry';
 import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 import { setCurrentUser } from './redux/user/user.actions';
+
 
 class App extends React.Component {
   unsubscribeFromAuth = null
@@ -43,7 +44,14 @@ class App extends React.Component {
       <Switch>
         <Route exact path="/" component={HomePage}/>
         <Route path="/shop" component={ShopPage}/>
-        <Route path="/signin" component={EntryPage}/>
+        <Route 
+          exact 
+          path="/signin" 
+          render={() => this.props.currentUser ? 
+            (<Redirect to ='/' />)
+            :
+            (<EntryPage/>)
+          }/>
 
       </Switch>
     </div>
@@ -51,8 +59,12 @@ class App extends React.Component {
 }
 }
 
+const mapStateToProps = ({user}) => ({
+  setCurrentUser: user.setCurrentUser
+});
+
 const mapDispatchToProps = dispatch => ({
   setCurrentUser: user => dispatch(setCurrentUser(user))
 });
 
-export default connect(null, mapDispatchToProps )(App);
+export default connect(mapStateToProps, mapDispatchToProps )(App);
---
+++ aa-store/src/component/cart-dropdown/cart-dropdown.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import CustomButton from '../button/custom-button';
+import './cart-dropdown.scss';
+import CartItem from '../cart-item/cart-item';
+import {connect} from 'react-redux';
+
+const CartDropdown = ({ cartItems }) => (
+    <div className='cart-dropdown'>
+      <div className='cart-items'>
+        {cartItems.map(cartItem => (
+          <CartItem key={cartItem.id} item={cartItem} />
+        ))}
+      </div>
+      <CustomButton>GO TO CHECKOUT</CustomButton>
+    </div>
+  );
+  
+  const mapStateToProps = ({ cart: { cartItems } }) => ({
+    cartItems
+  });
+  
+  export default connect(mapStateToProps)(CartDropdown);
