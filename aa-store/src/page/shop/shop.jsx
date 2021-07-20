b29afe4f09b15d85b6641a9b2a31e0a60dfefeb2
Jared Matta <cherokeedreemer@gmail.com>
2021-07-20 09:43:16 -0600

    adding collections to state

--- aa-store/src/page/shop/shop.jsx
+++ aa-store/src/page/shop/shop.jsx
@@ -1,27 +1,22 @@
 import React from 'react';
-import SHOP_DATA from './shop.data';
 import PreviewCollection  from '../../component/preview-collection/preview-collection';
+import { selectCollections } from '../../redux/shop/shop.selectors';
+import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
 
-class ShopPage extends React.Component {
-    constructor(props) {
-        super(props);
 
-        this.state = {
-            collections: SHOP_DATA
+const ShopPage = ({collections}) => (
+    <div className="shop-page">
+        {
+            collections.map(({id, ...otherCollectionProps }) => (
+                <PreviewCollection key={id} {...otherCollectionProps} />
+            ))
         }
-    }
+    </div>
+);
 
-    render() {
-        const {collections} = this.state;
+const mapStateToProps = createStructuredSelector({
+    collections: selectCollections
+})
 
-        return ( <div className="shop-page">
-            {
-                collections.map(({id, ...otherCollectionProps }) => (
-                    <PreviewCollection key={id} {...otherCollectionProps} />
-                ))
-            }
-        </div>);
-    }
-};
-
-export default ShopPage;
+export default connect(mapStateToProps)(ShopPage);
