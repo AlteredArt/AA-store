d268d1bd0d6b26b0c0a667d1d60fade6ef07dfb5
Jared Matta <cherokeedreemer@gmail.com>
2021-08-03 08:30:05 -0600

    styled componenets

--- aa-store/src/page/homepage/homepage.styles.jsx
+++ aa-store/src/page/homepage/homepage.styles.jsx
@@ -1,1 +1,8 @@
-import style from 'styled-components';
+import styled from 'styled-components';
+
+export const HomePageContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding: 20px 80px;
+`;
